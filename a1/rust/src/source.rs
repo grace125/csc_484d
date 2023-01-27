30c22ffe906b594ba077f78bc20f2dec1a9eb9f9
grace <graceschorno@protonmail.com>
2023-01-27 10:58:31 -0800

    A1: document/change api

--- a1/rust/src/source.rs
+++ a1/rust/src/source.rs
@@ -2,6 +2,7 @@ use rodio::Source;
 use std::time::Duration;
 use std::sync::Arc;
 
+/// A table which holds a waveform, and produces a [Source].
 #[derive(Clone)]
 pub struct WaveTable {
     data: Arc<Vec<f32>>,
@@ -10,13 +11,18 @@ pub struct WaveTable {
 
 impl WaveTable {
     
-    pub fn new(data: Vec<f32>, sample_rate: u32) -> WaveTable {
+    /// Creates a new wavetable from a collection of samples.
+    #[must_use]
+    pub fn new(data: impl IntoIterator<Item = f32>, sample_rate: u32) -> WaveTable {
         WaveTable { 
-            data: Arc::new(data), 
+            data: Arc::new(data.into_iter().collect()), 
             sample_rate 
         }
     }
 
+    /// Creates a new wavetable by sampling a function of the form `Fn(f32) -> f32`
+    /// between `0.0` and `1.0`.
+    #[must_use]
     pub fn from_function<T: SourceFunctionExt>(sample_rate: u32, function: T) -> WaveTable {
         let sample_rate_float = sample_rate as f32;
         WaveTable {
@@ -29,6 +35,8 @@ impl WaveTable {
         }
     }
 
+    /// Creates a [Source] from a wave table. 
+    #[must_use]
     pub fn source(&self) -> WaveTableSource {
         WaveTableSource {
             table: self.clone(),
@@ -40,26 +48,33 @@ impl WaveTable {
     /// Samples the wave table at a continuous value.
     ///
     /// `index` should never be negative.
+    #[must_use]
     pub fn sample(&self, index: f32) -> f32 {
         let left_index = index as usize;
         let right_index = (left_index + 1) % self.data.len();
         let weight = index % 1.0;
-        let lerp = self.data[left_index] * weight + self.data[right_index] * (1.0 - weight);
-        lerp
+        
+        self.data[left_index] * weight + self.data[right_index] * (1.0 - weight)
     }
 
+    #[must_use]
     pub fn total_duration(&self) -> Duration {
         Duration::from_secs_f32(self.data.len() as f32 / self.sample_rate as f32)
     }
 
+    #[must_use]
     pub fn sample_rate(&self) -> u32 {
         self.sample_rate
     }
 
+    /// Gives a reference to the underlying data of the table.
+    #[must_use]
     pub fn data(&self) -> &Vec<f32> {
         &self.data
     }
 }
+
+/// A [Source] of audio created by [WaveTable].
 pub struct WaveTableSource {
     table: WaveTable,
     index: f32,
@@ -71,6 +86,7 @@ impl WaveTableSource {
         self.increment = freq * self.table.data.len() as f32 / self.sample_rate() as f32;
     }
 
+    #[must_use]
     pub fn with_frequency(mut self, freq: f32) -> Self {
         self.set_frequency(freq);
         self
@@ -105,8 +121,12 @@ impl Source for WaveTableSource {
     }
 }
 
+/// An extension trait for functions of the form `Fn(f32) -> f32`.
+/// 
+/// This extension traits expects functions to be periodic between `0.0` and `1.0`,
+/// and for the function to output values between `-1.0` and `1.0`.
 pub trait SourceFunctionExt: Fn(f32) -> f32 + 'static + Sized {
-    ///  `0.0` to `1.0`
+    
     fn source(self, sample_rate: u32) -> FunctionSource<Self>;
 
     fn wavetable(self, sample_rate: u32) -> WaveTable {
@@ -125,6 +145,7 @@ impl<T: Fn(f32) -> f32 + 'static + Sized> SourceFunctionExt for T {
     }
 }
 
+/// A source of audio created by functions of the form `Fn(f32) -> f32`.
 pub struct FunctionSource<T: Fn(f32) -> f32 + 'static> {
     function: T,
     index: f32,
@@ -137,6 +158,7 @@ impl<T: SourceFunctionExt> FunctionSource<T> {
         self.increment = freq / self.sample_rate() as f32;
     }
 
+    #[must_use]
     pub fn with_frequency(mut self, freq: f32) -> Self {
         self.set_frequency(freq);
         self
--- a1/rust/src/wave.rs
+++ a1/rust/src/wave.rs
@@ -1,20 +1,25 @@
 // TODO: implement and benchmark
+#[must_use]
 pub fn sin(t: f32) -> f32 {
     (std::f32::consts::TAU * t).sin()
 }
 
+#[must_use]
 pub fn square(t: f32) -> f32 {
     if t < 0.5 { 1.0 } else { -1.0 }
 }
 
+#[must_use]
 pub fn triangle(t: f32) -> f32 {
     (if t < 0.5 { 4.0 } else { -4.0 }) * ((t % 0.5) - 0.25)
 }
 
+#[must_use]
 pub fn sawtooth(t: f32) -> f32 {
     (t - 0.5) * 2.0
 }
 
+#[must_use]
 pub fn noise(_t: f32) -> f32 {
     (rand::random::<f32>() - 0.5) * 2.0
 }
